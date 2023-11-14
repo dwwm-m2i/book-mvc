569b7306d0953c7f1bb8c765752c56564eba5b97
Matthieu Mota <matthieumota@gmail.com>
2023-11-14 11:52:53 +0100

    Fix

--- views/partials/footer.html.php
+++ views/partials/footer.html.php
@@ -1,5 +1,5 @@
     <div class="max-w-5xl mx-auto py-8 mt-8">
-        <p class="text-center">Copyright &copy; 2023</p>
+        <p class="text-center">Copyright &copy; <?= date('Y'); ?></p>
     </div>
 </body>
 </html>
